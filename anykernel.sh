9c8600e7fabe3d62001f6837dfc73cd15a75d057
Srinikethan <sriz~@gmail.com>
2020-07-07 13:23:23 +0200

    Updated shell script for ArcKernel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=ArcKernel by Srinikethan M
 do.devicecheck=1
 do.modules=0
 do.systemless=1
